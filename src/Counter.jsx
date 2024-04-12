cef50c07674c378c6f2f76b1b0e71a4886dc6947
dibya.borah <dibyajyotiborah469@gmail.com>
2024-04-12 22:12:55 +0530

    Re-practising 2nd time useReducer using. Making the reducer fc using if statement and also using switch case statement

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,46 +1,42 @@
 import { useReducer } from "react";
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case "DECREMENT":
-      return {
-        count: state.count - 1,
-      };
-    case "RESET":
-      return {
-        count: 0,
-      };
-    case "INCREMENT":
-      return {
-        count: state.count + 1,
-      };
-    default:
-      return state;
-  }
+  // using switch case:
 
-  // if(action.type === "DECREMENT"){
-  //     return {count: state.count - 1};
-  // }
-  // if(action.type === "RESET"){
-  //     return {count: 0};
-  // }
-  // if(action.type === "INCREMENT"){
-  //     return {count : state.count + 1};
-  // }
-  // return state;
+  //   switch (action.type) {
+  //     case "DECREMENT":
+  //       return { count: state.count - 1 };
+  //     case "RESET":
+  //       return { count: 0 };
+  //     case "INCREMENT":
+  //       return { count: state.count + 1 };
+  //     default:
+  //       return state;
+  //   }
+
+  // using if statement
+  if (action.type === "DECREMENT") {
+    return { count: state.count - 1 };
+  } else if (action.type === "RESET") {
+    return { count: 0 };
+  } else if (action.type === "INCREMENT") {
+    return { count: state.count + 1 };
+  } else {
+    return state;
+  }
 };
-const initialValue = {
+
+const initialState = {
   count: 0,
 };
-
 const Counter = () => {
-  const [state, dispatch] = useReducer(reducer, initialValue);
-  console.log(state);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const handleDecrease = () => {
     dispatch({
       type: "DECREMENT",
     });
   };
+
   const handleReset = () => {
     dispatch({
       type: "RESET",
@@ -52,8 +48,10 @@ const Counter = () => {
     });
   };
   return (
-    <div>
-      <h2>Counter: {state.count}</h2>
+    <div
+      style={{ backgroundColor: "#000", color: "#efefef", textAlign: "center" }}
+    >
+      <h1>{state.count}</h1>
       <button onClick={handleDecrease}>Decrease</button>
       <button onClick={handleReset}>Reset</button>
       <button onClick={handleIncrease}>Increase</button>
